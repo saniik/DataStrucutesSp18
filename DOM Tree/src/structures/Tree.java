f049bc2cdb3925d6b0b79e1847b896a6da3aea71
saniik <Sasha@Sanik.fios-router.home>
2018-03-15 15:21:57 -0400

    boldface

--- DOM Tree/src/structures/Tree.java
+++ DOM Tree/src/structures/Tree.java
@@ -82,21 +82,22 @@ public class Tree {
 	}
 
 	private void swapTag(TagNode tempRoot, String old, String newer){
-
-		if (tempRoot == null){
+		TagNode current = tempRoot;
+		if (current == null){
 			return;
 		}
 		System.out.println("TempRoot: " + tempRoot);
 
-		if ((tempRoot.tag.equals(old)) && tempRoot.firstChild != null){
+		if (current.tag.equals(old)){
 			System.out.println("Replacing "+ tempRoot.tag + " with " + newer);
-			tempRoot.tag = newer;
+			current.tag = newer;
 
 		}
 		System.out.println("TempRoot sibling:" + tempRoot.sibling);
 		System.out.println("TempRoot child: " + tempRoot.firstChild);
-		swapTag(tempRoot.sibling, old, newer);
 		swapTag(tempRoot.firstChild, old, newer);
+		swapTag(tempRoot.sibling, old, newer);
+
 	}
 	
 	/**
@@ -106,8 +107,51 @@ public class Tree {
 	 * @param row Row to bold, first row is numbered 1 (not 0).
 	 */
 	public void boldRow(int row) {
-		/** COMPLETE THIS METHOD **/
+		TagNode current = new TagNode(null, null, null);
+		TagNode temp;
+		current = boldrow2(root);
+		if (current == null) {
+			System.out.println("No table");
+			return;
+		}
+		current = current.firstChild;
+
+		//rows of table
+		for(int i = 1; i < row; i++) {
+			current = current.sibling;
+		}
+
+		//columns of table
+		for (temp = current.firstChild; temp != null; temp = temp.sibling) {
+			temp.firstChild = new TagNode("b", temp.firstChild, null);
+
+		}
+
 	}
+
+	private TagNode boldrow2(TagNode current) {
+		if (current == null)
+			return null;
+
+		TagNode tempNode = null;
+		String temp = current.tag;
+
+		if(temp.equals("table")) {
+			tempNode = current;
+			return tempNode;
+		}
+
+		if(tempNode == null) {
+			tempNode = boldrow2(current.firstChild);
+		}
+
+		if(tempNode == null) {
+			tempNode = boldrow2(current.sibling);
+		}
+		return tempNode;
+	}
+		
+	
 	
 	/**
 	 * Remove all occurrences of a tag from the DOM tree. If the tag is p, em, or b, all occurrences of the tag
